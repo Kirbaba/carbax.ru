034de063c471027822605dfb405a486a3b70ffa5
AKD47 <van83horn@gmail.com>
2016-06-10 16:43:45 +0300

    header slider fix

--- js/script.js
+++ js/script.js
@@ -122,4 +122,21 @@ $('.header__slider').owlCarousel({
         }
     }
 });
+$('.header__slider2').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
 /*close header-slider*/
